a360e4754e6b57b43210fcf097885bda420557ae
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2023-09-19 20:43:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+a = int(input("Введіть годину"))
+if 0<=a<=6:
+    print("Good night")
+elif 6<a<=13:
+    print("Good morning")
+elif 13<a<=18:
+    print("Good day")
+elif 18<=a<=0:
+    print("Good day")
+
+
